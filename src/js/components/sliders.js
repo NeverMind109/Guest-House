304ee7cfa90c6bde9b10463ad6d892ff5b802172
NeverMind109 <vladgladkih123@gmail.com>
2022-11-13 19:00:17 +0300

    Added some changes

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -4,6 +4,7 @@ Swiper.use([Navigation, Pagination]);
 const reviewsSlider = new Swiper(".reviews__slider", {
   slidesPerView: "auto",
   spaceBetween: 20,
+  grabCursor: true,
   navigation: {
     nextEl: ".reviews__slider-next",
     prevEl: ".reviews__slider-prev",
